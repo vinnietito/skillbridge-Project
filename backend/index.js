61dc7f37e88898bcff6521d8595ea753fb880872
vinnietito <vincentnjuguna69@gmail.com>
2025-03-20 22:06:09 +0300

    Commits

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "V@38080135k",
+    database: "skillbridge"
+});
